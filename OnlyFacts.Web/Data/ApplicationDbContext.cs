a10569feee3e1b906a7ea6ff796842ab577171ce
Sergey Nazhestkin <9497020@mail.ru>
2021-12-10 21:57:50 +0300

    A base class has been created to automatically add data (author and time) to an entity when it is created and edited.

--- OnlyFacts.Web/Data/ApplicationDbContext.cs
+++ OnlyFacts.Web/Data/ApplicationDbContext.cs
@@ -1,22 +1,19 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using System;
+using System.Linq;
+using Calabonga.EntityFrameworkCore.Entities.Base;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using OnlyFacts.Web.Data.Base;
 
 namespace OnlyFacts.Web.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : DbContextBase
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
 
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            builder.ApplyConfigurationsFromAssembly(typeof(Startup).Assembly);
-            base.OnModelCreating(builder);
-        }
+        public DbSet<Fact> Facts { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
     }
 }
---
+++ OnlyFacts.Web/Data/Base/DbContextBase.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Calabonga.EntityFrameworkCore.Entities.Base;
+using Calabonga.UnitOfWork;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace OnlyFacts.Web.Data.Base
+{
+    public abstract class DbContextBase : IdentityDbContext
+    {
+        public SaveChangesResult SaveChangesResult { get; set; }
+
+
+        protected DbContextBase(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            SaveChangesResult = new SaveChangesResult();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(typeof(Startup).Assembly);
+            base.OnModelCreating(builder);
+        }
+
+        public override int SaveChanges()
+        {
+            DbSaveChanges();
+            return base.SaveChanges();
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            DbSaveChanges();
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            DbSaveChanges();
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = new CancellationToken())
+        {
+            DbSaveChanges();
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        private void DbSaveChanges()
+        {
+            // Added
+
+            const string defaultUser = "System";
+            var defaultDate = DateTime.UtcNow;
+
+            var addedEntities = ChangeTracker.Entries().Where(x => x.State == EntityState.Added);
+
+            foreach (var entry in addedEntities)
+            {
+                if (!(entry.Entity is IAuditable))
+                {
+                    continue;
+                }
+
+                var createdAt = entry.Property(nameof(IAuditable.CreatedAt)).CurrentValue;
+                var createdBy = entry.Property(nameof(IAuditable.CreatedBy)).CurrentValue;
+                var updatedAt = entry.Property(nameof(IAuditable.UpdatedAt)).CurrentValue;
+                var updatedBy = entry.Property(nameof(IAuditable.UpdatedBy)).CurrentValue;
+
+                if (string.IsNullOrEmpty(createdBy?.ToString()))
+                {
+                    entry.Property(nameof(IAuditable.CreatedBy)).CurrentValue = defaultUser;
+                }
+
+                if (string.IsNullOrEmpty(updatedBy?.ToString()))
+                {
+                    entry.Property(nameof(IAuditable.UpdatedBy)).CurrentValue = defaultUser;
+                }
+
+                if (DateTime.Parse(createdAt?.ToString()!).Year < 1970)
+                {
+                    entry.Property(nameof(IAuditable.CreatedAt)).CurrentValue = defaultDate;
+                }
+
+                if (updatedAt != null && DateTime.Parse(updatedAt.ToString()!).Year < 1970)
+                {
+                    entry.Property(nameof(IAuditable.UpdatedAt)).CurrentValue = defaultDate;
+                }
+                else
+                {
+                    entry.Property(nameof(IAuditable.UpdatedAt)).CurrentValue = defaultDate;
+                }
+
+                SaveChangesResult.AddMessage("Some entities were created");
+            }
+
+            // Modified
+
+            var modifiedEntities = ChangeTracker.Entries().Where(x => x.State == EntityState.Modified);
+
+            foreach (var entry in modifiedEntities)
+            {
+                if (entry.Entity is IAuditable)
+                {
+                    var userName = entry.Property(nameof(IAuditable.UpdatedBy)).CurrentValue == null
+                        ? defaultUser
+                        : entry.Property(nameof(IAuditable.UpdatedBy)).CurrentValue;
+
+                    entry.Property(nameof(IAuditable.UpdatedAt)).CurrentValue = DateTime.UtcNow;
+                    entry.Property(nameof(IAuditable.UpdatedBy)).CurrentValue = userName;
+                }
+
+                SaveChangesResult.AddMessage("Some entities were modified");
+            }
+        }
+    }
+}
